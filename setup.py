b157890f0ceddf65ed7e7614161ef5bb79320365
Niko Fink <nfink95@gmail.com>
2017-11-10 15:57:22 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ from setuptools import setup
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    with open("studip/__init__.py", "r") as file:
+    with open("studip_api/__init__.py", "r") as file:
         version = re.search('^__version__\s*=\s*"(.*)"', file.read(), re.M).group(1)
 
-    with open("README", "rb") as f:
+    with open("README.md", "rb") as f:
         long_descr = f.read().decode("utf-8")
 
     setup(
