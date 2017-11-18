41947082028efdebfe3a0f7f3323650c5a6f9b33
Niko Fink <nfink95@gmail.com>
2017-11-18 15:07:48 +0100

    add missing dependencies

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
             "aiofiles",
             "aiohttp",
             "beautifulsoup4",
+            "lxml",
             "cached_property",
         ],
         version=version,
